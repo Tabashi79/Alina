4d714424afe589b8e6491c33c146b7d8e7f307cb
Tabashi kun <103929154+Tabashi79@users.noreply.github.com>
2022-08-10 10:45:33 +0000

    new fixed

--- alina.js
+++ alina.js
@@ -9,7 +9,6 @@ const { VK, Context } = require('vk-io');
 const { HearManager } = require('@vk-io/hear');
 const wiki = require('wikipedia');
 const { content } = require('wikipedia');
-//var pageviews = require('pageviews');
 
 
 app.get('/', function(request, response) { response.send(`Монитор активен. Локальный адрес: http://localhost:${port}`); });
@@ -20,52 +19,59 @@ const vk = new VK({
 })
 const bot = new HearManager();
 
-// Функция с работой вики
-const f = (async () => {
+// Случайное число от 1 до 100, для игры в Угадай число
+let numberRandom = Math.round(Math.random() * 99) + 1;
+// Попытки пользователя
+let attemptsUser = 0;
+
+
+// Функция
+const main = (async () => {
   try {
     vk.updates.on('message_new', async (context) => {
-      const changedLang = await wiki.setLang('ru');
-      const page = await wiki.page(`${context.text}`);
-      const summary = await page.summary();
-      //console.log(summary.extract);
-      //console.log(summary.content_urls.desktop.page);
-      await context.send(summary.extract);
-      await context.send(`Вот ссылка: ${summary.content_urls.desktop.page}`);
-      if (context.senderId !== 593658628) {
-        console.log(`Мне опять написал этот бака id${context.senderId}: ${context.text}`);
-    }
-  });
-} catch (error) {
-  console.log(error);
-  //=> Typeof wikiError
-}
+      // Википедия
+      if (context.text.substr(0, 5) === '-wiki') {
+        const changedLang = await wiki.setLang('ru');
+        const page = await wiki.page(`${context.text.substr(6)}`);
+        const summary = await page.summary();
+        await context.send(summary.extract);
+        await context.send(`Вот ссылка: ${summary.content_urls.desktop.page}`);
+        if (context.senderId !== 593658628) {
+          console.log(`Мне опять написал этот бака id${context.senderId}: ${context.text}`);
+        }
+      }
+
+      // Игра угадай число
+      if (context.text.substr(0, 3) === '-gn') {
+        if (parseInt(context.text.substr(4)) > numberRandom) {
+          await context.send('Меньше...');
+          attemptsUser++;
+        }
+        else if (parseInt(context.text.substr(4)) < numberRandom) {
+          await context.send('Больше...');
+          attemptsUser++;
+        }
+        else if (parseInt(context.text.substr(4)) === numberRandom) {
+          await context.send(`Вы угадали с ${attemptsUser} попытки. Это число ${numberRandom}.`);
+          // Новное случайное число, чтоб не перезапускать бота каздый раз для нового числа
+          numberRandom = Math.round(Math.random() * 99) + 1;
+        }
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    //=> Typeof wikiError
+  }
 })
 
 vk.updates.on('message_new', bot.middleware);
-// ${msg.senderId}
+
 bot.hear(/привет/i, msg => {
   msg.send('Здравствуйте, я пока мало что умею, только вывести это сообшение и работать с википедией, надеюсь мой хозяин будет меня обновлять...');
   msg.send('Я смотрела аниме "Сонни Бой", советую и вам посмотреть ^_^');
 });
 
-let numRandom = 0;
-
-/*
-bot.hear(/угадай число/i, msg => {
-  numRandom = Math.floor(Math.random * 99) + 1;
-  msg.send('Я загадаю число от 1 до 100 включительно, а вы попробуете отгадать ^_^');
-  msg.send('Введите число.');
-
-  if (numRandom > parseInt(context)) {
-    msg.send("Больше...");
-  }
-  else if (numRandom < parseInt(context)) {
-    msg.send("Меньше...");
-  }
-  })
-*/
-
-f();
+main();
 
 console.log('Бот запущен!');
 vk.updates.start().catch(console.error);
